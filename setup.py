a05dc92d558e192968189bb046866922d22677fa
Carl Meyer <carl@dirtcircle.com>
2009-03-08 15:38:58 -0400

    initial import
    --HG--
    extra : convert_revision : carl%40dirtcircle.com-20090308193858-yyxyf27s00lytfkm

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-markitup',
+    version='0.1.0',
+    description='Django integration with the MarkItUp universal markup editor',
+    author='Carl Meyer',
+    author_email='carl@dirtcircle.com',
+    url='http://launchpad.net/django-markitup',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools'],
+)
