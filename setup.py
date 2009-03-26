7cfeb0064ab1ebb6aff127fbb80816b325eb58bc
Carl Meyer <carl@dirtcircle.com>
2009-03-26 17:08:47 -0500

    bump version to 0.3.0pre
    --HG--
    extra : convert_revision : carl%40dirtcircle.com-20090326220847-94uzysfq6hpa3dat

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-markitup',
-    version='0.2.0',
+    version='0.3.0pre',
     description='Django integration with the MarkItUp universal markup editor',
     author='Carl Meyer',
     author_email='carl@dirtcircle.com',
