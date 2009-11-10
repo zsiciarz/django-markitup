ab808e2456418d792006ed2c62dbf84cd83cf822
Carl Meyer <carl@dirtcircle.com>
2009-11-10 01:05:49 -0500

    fix HGREV so it uses actual rev of working tree

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import os.path
 try:
     # don't get confused if our sdist is unzipped in a subdir of some 
     # other hg repo
-    if os.path.isdir('./.hg'):
-        p = subprocess.Popen(['hg', 'log', '-l1', '--template={rev}'],
+    if os.path.isdir('.hg'):
+        p = subprocess.Popen(['hg', 'parents', r'--template={rev}\n'],
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if not p.returncode:
             fh = open('HGREV', 'w')
-            fh.write(p.communicate()[0])
+            fh.write(p.communicate()[0].splitlines()[0])
             fh.close()
-except OSError:
+except (OSError, IndexError):
     pass
     
 try:
