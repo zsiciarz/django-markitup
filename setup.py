510281204831e1b56ad640fd22ebb079623250c5
Carl Meyer <carl@dirtcircle.com>
2009-11-25 00:09:30 -0500

    bump version for dev

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ long_description = (open('README.txt').read() +
 
 setup(
     name='django-markitup',
-    version='0.5.2',
+    version='0.5.2.post%s' % hgrev,
     description='Markup handling for Django using the MarkItUp! universal markup editor',
     long_description=long_description,
     author='Carl Meyer',
