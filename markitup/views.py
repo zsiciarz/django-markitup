e5b9a6d328ecddfa4580259a9570467b0399b0f0
Carl Meyer <carl@dirtcircle.com>
2009-12-09 21:38:22 -0500

    use RequestContext for preview view

--- markitup/views.py
+++ markitup/views.py
@@ -5,12 +5,13 @@ Time-stamp: <2009-11-06 02:26:59 carljm views.py>
 
 """
 from django.shortcuts import render_to_response
-from markitup import settings
+from django.template import RequestContext
 
+from markitup import settings
 from markitup.markup import filter_func
 
 def apply_filter(request):
     markup = filter_func(request.POST.get('data', ''))
     return render_to_response( 'markitup/preview.html',
-                              {'preview':markup,
-                               'MEDIA_URL': settings.MARKITUP_MEDIA_URL.rstrip('/')})
+                              {'preview':markup},
+                              context_instance=RequestContext(request))
