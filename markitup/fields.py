b80a4906fe192f4f6fb2b82eb84aedf449759cbb
Carl Meyer <carl@oddbird.net>
2011-04-28 17:09:21 -0500

    Removed unnecessary creation_counter incrementing in MarkupField. Thanks Denis Kolodin.

--- markitup/fields.py
+++ markitup/fields.py
@@ -77,7 +77,6 @@ class MarkupField(models.TextField):
     def contribute_to_class(self, cls, name):
         if self.add_rendered_field and not cls._meta.abstract:
             rendered_field = models.TextField(editable=False, blank=True)
-            rendered_field.creation_counter = self.creation_counter+1
             cls.add_to_class(_rendered_field_name(name), rendered_field)
         super(MarkupField, self).contribute_to_class(cls, name)
         setattr(cls, self.name, MarkupDescriptor(self))
