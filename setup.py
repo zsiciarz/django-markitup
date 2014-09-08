eef475ed61aa434d614c5ca2aa15d49bf74f681f
Carl Meyer <carl@oddbird.net>
2014-09-08 16:22:48 -0600

    Bump version to 2.2.2 post.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def _static_files(prefix):
 
 setup(
     name='django-markitup',
-    version='2.2.2',
+    version='2.2.2.post%s' % hgrev,
     description='Markup handling for Django using the MarkItUp! universal markup editor',
     long_description=long_description,
     author='Carl Meyer',
