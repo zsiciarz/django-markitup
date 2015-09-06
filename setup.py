d9d3ae4a1d4007a0aa1dafe09102cb7414c338db
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-09-06 10:41:46 +0200

    Remove extracting HG revision from setup.py.

--- setup.py
+++ setup.py
@@ -1,24 +1,5 @@
 from setuptools import setup
-import subprocess
-import os.path
 
-try:
-    # don't get confused if our sdist is unzipped in a subdir of some
-    # other hg repo
-    if os.path.isdir('.hg'):
-        p = subprocess.Popen(['hg', 'parents', r'--template={rev}\n'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        if not p.returncode:
-            fh = open('HGREV', 'wb')
-            fh.write(p.communicate()[0].splitlines()[0])
-            fh.close()
-except (OSError, IndexError):
-    pass
-
-try:
-    hgrev = open('HGREV').read()
-except IOError:
-    hgrev = ''
 
 long_description = (open('README.rst').read() +
                     open('CHANGES.rst').read() +
@@ -36,7 +17,7 @@ def _static_files(prefix):
 
 setup(
     name='django-markitup',
-    version='2.2.2.post%s' % hgrev,
+    version='2.2.2.post0',
     description='Markup handling for Django using the MarkItUp! universal markup editor',
     long_description=long_description,
     author='Carl Meyer',
