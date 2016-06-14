2cf7f70e352f8427cfb7d1dba309ee7d7e0ce5f4
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-06-14 20:53:03 +0200

    Use plain Python list for urlpatterns.

--- markitup/urls.py
+++ markitup/urls.py
@@ -1,10 +1,9 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from markitup.views import apply_filter
 
-urlpatterns = patterns(
-    '',
-    url(r'preview/$', apply_filter, name='markitup_preview')
-    )
+urlpatterns = [
+    url(r'preview/$', apply_filter, name='markitup_preview'),
+]
