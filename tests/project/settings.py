d0901a36de4d7ef71bf615131f48e6333d93c2b0
Zbigniew Siciarz <zbigniew@siciarz.net>
2016-06-14 21:06:05 +0200

    Use TEMPLATES setting in tests.

--- tests/project/settings.py
+++ tests/project/settings.py
@@ -7,7 +7,26 @@ INSTALLED_APPS = [
     'markitup',
     ]
 
-TEMPLATE_DIRS = [join(BASE_DIR, 'templates')]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 ROOT_URLCONF = 'tests.project.urls'
 
